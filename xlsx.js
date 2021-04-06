a872cb04d6f2679fb2cdc356e63e812d2c494491
Aaron Dix <aaron.dix@bettrdata.io>
2021-04-06 08:25:44 -0600

    Add regex for catching scientific notation

--- xlsx.js
+++ xlsx.js
@@ -21215,7 +21215,8 @@ function make_json_row(sheet, r, R, cols, header, hdr, dense, o) {
 				else if(raw && v === null) row[hdr[C]] = null;
 				else continue;
 			} else {
-				row[hdr[C]] = (raw && val.t != 'd') || (o.rawNumbers && val.t == "n") ? v : format_cell(val,v,o);
+                const snRegex = /^([0-9]+[.]?[0-9]*|[0-9]*[.][0-9]+)[eE][+-]?[0-9]+$/;
+                row[hdr[C]] = (val.w && val.w.match(snRegex)) || raw || (o.rawNumbers && val.t == "n") ? v : format_cell(val,v,o);
 			}
 			if(v != null) isempty = false;
 		}
